76eba1ea101b5921d23d8fa41bc4ccc0ae124111
NabokovSG <nabokovsg@yandex.ru>
2023-04-28 15:39:28 +0300

    add service

---
+++ DocLab/PassportService/src/main/java/ru/nabokov/passportservice/service/PassportService.java
@@ -0,0 +1,18 @@
+package ru.nabokov.passportservice.service;
+
+import ru.nabokov.passportservice.dto.passport.*;
+import ru.nabokov.passportservice.model.Passport;
+import java.util.List;
+
+public interface PassportService {
+
+    PassportDto save(NewPassportDto passportDto);
+
+    PassportDto update(UpdatePassportDto passportDto);
+
+    Passport get(Long id);
+
+    List<Passport> getAll();
+
+   void delete(Long id);
+}
